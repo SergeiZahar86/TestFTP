a22a347b348d24eefa406d25918772c1040a4bcd
Сергей Захаров <sergei86zahar@mail.ru>
2021-01-11 17:54:25 +0700

    1

--- TestFTP/Program.cs
+++ TestFTP/Program.cs
@@ -1,20 +1,25 @@
 ﻿using EnterpriseDT.Net.Ftp;
 using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 
 namespace TestFTP
 {
     class Program
     {
+        public static List<string> Local_long_listFiles;
         static void Main(string[] args)
         {
-            string dirName = "C:/VisualStudioProject/TestFTP";
+            Local_long_listFiles = new List<string>();
+            string dirName = ConfigurationManager.AppSettings["dirName"];
+
 
             if (Directory.Exists(dirName))
             {
                 Console.WriteLine("Подкаталоги:");
-                string[] dirs = Directory.GetDirectories(dirName);
-                foreach (string s in dirs)
+                string[] dirs_ = Directory.GetDirectories(dirName);
+                foreach (string s in dirs_)
                 {
                     Console.WriteLine(s);
                     Console.WriteLine();
@@ -23,23 +28,85 @@ namespace TestFTP
                     foreach (string p in files)
                     {
                         Console.WriteLine(p);
+                        Local_long_listFiles.Add(p);
                     }
                 }
             }
-            Console.Read();
 
 
-            /*FTPConnection ftp = new FTPConnection();
+            FTPConnection ftp = new FTPConnection();
             ftp.ConnectMode = FTPConnectMode.ACTIVE;
-            ftp.ServerAddress = "10.90.90.58";
-            ftp.UserName = "incube";
-            ftp.Password = "incube";
+            ftp.ServerAddress = ConfigurationManager.AppSettings["ServerAddress"];
+            ftp.UserName = ConfigurationManager.AppSettings["UserName"];
+            ftp.Password = ConfigurationManager.AppSettings["UserName"];
 
             ftp.Connect();
-            ftp.CreateDirectory("testDir");
-            ftp.UploadFile("C:/Users/Sergei/Desktop/C#/NET/TestFTP/TestFTP/bin/Debug/test.txt", "/testDir/test.txt", true);
+            try
+            {
+                ftp.CreateDirectory("testDir");
+            }
+            catch
+            {
+
+            }
+            //ftp.UploadFile("C:/VisualStudioProject/TestFTP/NET/TestFTP/TestFTP/bin/Debug/test.txt", "/testDir/test.txt", true);
             //ftp.DownloadFile("test.txt", "testDir/test.txt");
-            ftp.Close();*/
+            string[] fileDetails = ftp.GetFiles();
+            foreach(string a in fileDetails)
+            {
+                Console.WriteLine(a);
+                if(a == "./testDir")
+                {
+                    Console.WriteLine("папка testDir");
+                }
+            }
+            FTPFile[] filesFTP = ftp.GetFileInfos();
+            foreach(FTPFile d in filesFTP)
+            {
+                Console.WriteLine(d);
+                if(d.Name == "testDir")
+                {
+                    Console.WriteLine("папка testDir");
+                    ftp.ChangeWorkingDirectory(d.Name);
+                    //foreach()
+                    FTPFile[] files__FTP = ftp.GetFileInfos();
+                    foreach(FTPFile g in files__FTP)
+                    {
+                        Console.WriteLine(g);
+                    }
+
+                }
+            }
+            ftp.Close();
+
+            // Список локальных директорий
+            /*string[] dirs = Directory.GetDirectories(@"D:\\APP", "*", SearchOption.TopDirectoryOnly);
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                dirs[i] = new FileInfo(dirs[i]).Name; // Выделяем короткое название из пути
+            }
+
+            // Список удаленных директорий
+            FTPFile[] file__Details = ftp.GetFileInfos("");
+            foreach (FTPFile file in file__Details)
+            {
+                if (file.Dir && Array.Exists(dirs, x => x == file.Name))
+                {
+                    Console.WriteLine(file.Name + " " + file.Dir);
+                }
+
+                //Console.WriteLine(file.Name + " " + file.Dir);
+            }*/
+
+
+
+
+            
+
+
+
+
+            Console.Read();
 
         }
     }
